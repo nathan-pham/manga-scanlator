b2fc4196053742f29b57eef604eafa3fb8c4b86b
Nathan Pham <nathanpham.me@gmail.com>
2021-06-23 16:41:53 -0700

    switch to bs4

--- automate/main.py
+++ automate/main.py
@@ -1,36 +1,26 @@
-from selenium import webdriver
-import requests
-import os
-
-from automate.options import default_options
-from automate.wait import wait
-
-driver = webdriver.Chrome(
-    executable_path="C:/Users/nathan-pham/python-scripts/chromedriver.exe", 
-    options=default_options
-)
+from bs4 import BeautifulSoup
+import requests, os
 
 # "https://tonarinoyj.jp/episode/3269632237330300439"
 
-def quit_driver():
-    driver.quit()
-
-def get_json(manga_site):
-    driver.get(manga_site)
-
-    viewer_selector = "#page-viewer > section.viewer.js-viewer"
-    wait(driver, viewer_selector)
-    viewer = driver.find_element_by_css_selector(viewer_selector)
-    manga_api = viewer.get_attribute("data-json-url").strip()
-
-    res = requests.get(manga_api, headers={
-        "content-type": "application/json",
+def fetch(url, _from=None):
+    return requests.get(url, headers={
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
-        "referer": manga_site,
-        "origin": manga_site,
+        "referer": _from,
+        "origin": _from,
     })
 
+def get_json(manga_site):
+    viewer_selector = "#page-viewer > section.viewer.js-viewer"
+    
+    res = fetch(manga_site, manga_site)
     if res.status_code == 200:
-        return res.json()
-    else:
-        return { "error": res.status_code }
+        soup = BeautifulSoup(res.text, "html.parser")
+        viewer_element = soup.select(viewer_selector)[0]
+        manga_api = viewer_element.get("data-json-url").strip()
+
+        res = fetch(manga_api)
+        if res.status_code == 200:
+            return res.json()
+
+    return {}
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from PIL import Image, ImageDraw, ImageEnhance, ImageOps, ImageFilter
+from googletrans import Translator
 import requests, json, math, os
 import pytesseract
+import cv2
 
-from automate.main import get_json, quit_driver
+# from automate.main import get_json, quit_driver
+from translate.locate_bubbles import get_blurbs
 from img_map import img_map
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
@@ -48,8 +51,6 @@ def unscramble(filename, show=False, download=False):
 
 
     for y, x, cropped_img in all_crops:
-        # nx = math.floor(y / height_increment)
-        # ny = math.floor(x / width_increment)
         new_img.paste(cropped_img, (
             x * width_increment, 
             y * height_increment # + (x * 5) # - (y * 15) # - math.floor(y * scale_factor[x] * 5)
@@ -86,24 +87,72 @@ def export_pdf(in_url, out_pdf="./manga.pdf"):
     return (img1, out_pdf)
 
 # export_pdf("https://tonarinoyj.jp/episode/3269632237330300439")
-img = unscramble("./images/3269754496293452143-0104389836574dd726d44eaf904c4b1f.jpeg")
-img = img.convert('L').filter(ImageFilter.MedianFilter()).point(lambda x : 0 if x < 140 else 255)
+# img = unscramble("./images/3269754496293452143-0104389836574dd726d44eaf904c4b1f.jpeg")
+img = Image.open("./bruh.jpeg")
+draw = ImageDraw.Draw(img)
+
+with open("./format.json") as f:
+    data = json.load(f)
+
+lines = data["ParsedResults"][0]["TextOverlay"]["Lines"]
+translator = Translator()
+
+for line in lines:
+    words = line["Words"]
+
+    for word in words:
+        draw.rectangle([word["Left"], word["Top"], word["Left"] + word["Width"], word["Top"] + word["Height"]], outline="red", width=5)
+
+    translated = translator.translate(line["LineText"])
+    text = translated.text
+    
+    draw.text([ words[0]["Left"], words[0]["Top"] ], fill="white", text=text.encode("ascii", "ignore"), stroke_width="red", stroke_fill="red")
+
+# >>> 
+# >>> translator = Translator()
+# >>> translator.translate('안녕하세요.')
+
+
 img.show()
-
-boxes = pytesseract.image_to_boxes(img, "jpn").strip().split('\n')
-
 """
-import sys
-IMAGE_PATH = sys.argv[1]
-
-# open image
-im = Image.open(IMAGE_PATH)
-
-# preprocessing
-im = im.convert('L')                             # grayscale
-im = im.filter(ImageFilter.MedianFilter())       # a little blur
-im = im.point(lambda x: 0 if x < 140 else 255)   # threshold (binarize)
-
-text = pytesseract.image_to_string(im)           # pass preprocessed image to tesseract
-print(text)                
+                        "LineText": "\u66f4\u65b0\u4e2d\u2033\u30fb",
+                        "MaxHeight": 17.0,
+                        "MinTop": 1082.0,
+                        "Words": [
+                            {
+                                "Height": 16.0,
+                                "Left": 29.0,
+                                "Top": 1083.0,
+                                "Width": 16.0,
+                                "WordText": "\u66f4"
+                            },
+                            {
+                                "Height": 16.0,
+                                "Left": 46.0,
+                                "Top": 1083.0,
+                                "Width": 16.0,
+                                "WordText": "\u65b0"
+                            },
+                            {
+                                "Height": 17.0,
+                                "Left": 63.0,
+                                "Top": 1082.0,
+                                "Width": 16.0,
+                                "WordText": "\u4e2d"
+                            },
+                            {
+                                "Height": 13.0,
+                                "Left": 81.0,
+                                "Top": 1084.0,
+                                "Width": 14.0,
+                                "WordText": "\u2033"
+                            },
+                            {
+                                "Height": 3.0,
+                                "Left": 95.0,
+                                "Top": 1091.0,
+                                "Width": 4.0,
+                                "WordText": "\u30fb"
+                            }
+                        ]
 """
